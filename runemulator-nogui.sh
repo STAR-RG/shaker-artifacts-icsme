70bed7da8855f328e7c5ae915bf9f7230e8d4170
Leopoldo Teixeira <lmt@cin.ufpe.br>
2020-08-18 11:41:54 -0300

    polishing emulator scripts

--- runemulator-nogui.sh
+++ runemulator-nogui.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ $# -eq 0 ] then
+if [ $# -eq 0 ]; then
     ./runemulator.sh d no-gui 
 else
-    if [ $# -eq 1 ] then
+    if [ $# -eq 1 ]; then
         ./runemulator.sh $1 no-gui & 
     fi
 fi
--- runemulator.sh
+++ runemulator.sh
@@ -7,13 +7,13 @@ fi
 
 adb start-server
 
-if [ $# -eq 0 ] then
+if [ $# -eq 0 ]; then
     emulator -avd d -no-accel -no-boot-anim & 
 else
-    if [ $# -eq 1 ] then
+    if [ $# -eq 1 ]; then
         emulator -avd $1 -no-accel -no-boot-anim & 
     else
-        if [ $# -eq 2 ] then
+        if [ $# -eq 2 ]; then
             emulator -avd $1 -no-accel -no-boot-anim -no-window & 
         fi
     fi
