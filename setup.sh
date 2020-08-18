f33b0eac29b32038ccd6bad5fb347cf7dec77853
Leopoldo Teixeira <lmt@cin.ufpe.br>
2020-08-17 23:26:52 -0300

    removing download apk step, since we are adding APKs to the repository

--- setup.sh
+++ setup.sh
@@ -26,27 +26,3 @@ yes | ./Android/cmdline-tools/tools/bin/sdkmanager --licenses
 ./Android/cmdline-tools/tools/bin/sdkmanager "system-images;android-28;default;x86"
 ./Android/cmdline-tools/tools/bin/sdkmanager "build-tools;28.0.3"
 echo no | ./Android/cmdline-tools/tools/bin/avdmanager create avd --name d --package "system-images;android-28;default;x86"
-
-#Download APKs
-# AntennaPod
-wget --no-check-certificate https://drive.google.com/uc?export=download&id=12_vjyZehbBpRHeoxCy2WVCGZBAQA0Kd8 AntennaPod.apk
-# AnyMemo
-wget https://drive.google.com/uc?export=download&id=1SgyyoCPLjWQDPSWcZKWdGWTvy3bJ50yv
-# Espresso
-wget https://drive.google.com/uc?export=download&id=1WASqSL854zTG1cppz-Iys9bZq1I1VJQn
-# FirefoxLite
-wget https://drive.google.com/uc?export=download&id=1Uin8q9lRdx-0ShHOAPkT2KqBiSvQmuQr
-# flexbox
-wget https://drive.google.com/uc?export=download&id=1sObUYlPyxtjhHWROwi0aJ3U73ILn4CN3
-# KISS
-wget https://drive.google.com/uc?export=download&id=1IIdqiwjoh9iwraB3AgtzgjZ3qMVle8w_
-# OmniNotes
-wget https://drive.google.com/uc?export=download&id=1cT1n1bzhAX6T4odYp2-W08kDz0rB31Im
-# Orgzly
-wget https://drive.google.com/uc?export=download&id=18T7S3ki2x_N0nu-xRUW3dxWHukrgueac
-# Paintroid
-wget https://drive.google.com/uc?export=download&id=1GwoI2cEgan-FPyu697cU3FdD5_FeCveM
-# susi
-wget https://drive.google.com/uc?export=download&id=1pYbS8TdLFe5Dczjojm79BItgZFcY9Zn5
-# WifiAnalyzer
-wget https://drive.google.com/uc?export=download&id=17AXcaB-jKGV5b0Q0zC6uSPOkMeK-_iEF
