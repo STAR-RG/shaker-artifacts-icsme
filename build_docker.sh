02f6a23a54bb250917b0bbc308d8c3b3945d2687
denini08 <denini.gabriel@gmail.com>
2020-09-16 17:54:14 -0300

    updade script

--- build_docker.sh
+++ build_docker.sh
@@ -2,6 +2,6 @@
 docker build -t shaker .
 docker run --privileged -it shaker bash -c "./e.sh && ./install-apps.sh"
 docker commit shaker:latest shaker:latest
-h=$(docker ps -a | grep "shaker" | head -1 | cut -f1 -d" ")
+h=$(docker ps -a | grep -vE "CONTAINER ID" | head -1 | cut -f1 -d" ")
 docker commit $h shaker:latest
 echo "shaker:latest added successfully. To enter in the container type: 'docker run --privileged -it shaker'"
