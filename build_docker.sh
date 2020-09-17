4b179133de3030e229aeaee9c9cca4eda69cba78
denini08 <denini.gabriel@gmail.com>
2020-09-17 00:25:31 -0300

    fixing bugs

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 docker build -t shaker .
-docker run --privileged -it shaker bash -c "./e.sh && ./install-apps.sh"
-docker commit shaker:latest shaker:latest
+docker run --privileged shaker bash -c "./e.sh && ./install-apps.sh && adb emu kill && sleep 10"
 h=$(docker ps -a | grep -vE "CONTAINER ID" | head -1 | cut -f1 -d" ")
-docker commit $h shaker:latest
-echo "shaker:latest added successfully. To enter in the container type: 'docker run --privileged -it shaker'"
+docker commit $h shaker
+echo "shaker:latest added successfully. To enter in the container type: 'docker run --privileged -it shaker /bin/bash'"
